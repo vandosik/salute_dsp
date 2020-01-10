9ee86dfff949b8e71af463af9468524c90c644a3
Ivan Dubinin <i.dubinin@kpda.ru>
2020-01-10 16:44:21 +0300

    Regs decribing is almost finished

--- src/hardware/delcore30m/elcore/arm/mcom.le.v7/mcom_dsp.h
+++ src/hardware/delcore30m/elcore/arm/mcom.le.v7/mcom_dsp.h
@@ -5,7 +5,7 @@
 
 #include <elcore-manager.h>
 
-#define DLCR30M_MAX_CORES				2
+#define DLCR30M_MAX_CORES					2
 
 #define DLCR30M_BASE						0x37000000UL
 #define DLCR30M_END							0x3A87FFFCUL
@@ -57,6 +57,105 @@
 #define DLCR30M_DSP1_PRAM_PHYS				0x3A620000UL
 #define DLCR30M_DSP1_PRAM					(DLCR30M_DSP1_PRAM_PHYS - DLCR30M_BASE)
 #define DLCR30M_DSP1_PRAM_SIZE				0x00008000
+
+
+//------------------ Delcore common regs --------------------------------//
+
+
+#define DLCR30M_MASKR							DLCR30M_CMN_REGS	//R/W
+
+#define DLCR30M_QSTR							(DLCR30M_CMN_REGS + 4)	//R
+	#define DLCR30M_QSTR_PI(core_num)				(1<<(8*core_num))
+	#define DLCR30M_QSTR_SE(core_num)				(1<<(1 + 8*core_num))
+	#define DLCR30M_QSTR_BREAK(core_num)			(1<<(2 + 8*core_num))
+	#define DLCR30M_QSTR_STP(core_num)				(1<<(3 + 8*core_num))
+	#define DLCR30M_QSTR_dBREAK(core_num)			(1<<(4 + 8*core_num))
+	#define DLCR30M_QSTR_WAIT						(1<<5)
+	#define DLCR30M_QSTR_INT_MEM_ERR 				(1<<6)
+
+#define DLCR30M_INTR_PI_0						0
+#define DLCR30M_INTR_SE_0						1
+#define DLCR30M_INTR_BREAK_0					2
+#define DLCR30M_INTR_STP_0						3
+#define DLCR30M_INTR_PI_1						8
+#define DLCR30M_INTR_SE_1						9
+#define DLCR30M_INTR_BREAK_1					10
+#define DLCR30M_INTR_STP_1						11
+#define DLCR30M_INTR_WAIT						28
+
+#define DLCR30M_CSR								(DLCR30M_CMN_REGS + 8)	//R/W
+	#define DLCR30M_CSR_SYNC_START					(1 << 0)
+	#define DLCR30M_CSR_SYNC_WORK					(1 << 1)
+	#define DLCR30M_CSR_PM_CONFIG(val)				((val & 0x3) << 2)
+	#define DLCR30M_CSR_HEN							(1 << 16)
+	#define DLCR30M_CSR_DEN							(1 << 17)
+	#define DLCR30M_CSR_LEN							(1 << 18)
+	#define DLCR30M_CSR_DPTR(val)					((val & 0x3) << 20)
+	#define DLCR30M_CSR_LIMIT(val)					((val & 0x3F) << 24)
+
+#define DLCR30M_TOTAL_RUN						(DLCR30M_CMN_REGS + 12)	//R/W
+
+#define DLCR30M_TOTAL_CLK						(DLCR30M_CMN_REGS + 16)	//R/W
+
+//------------------ elcore core regs --------------------------------//
+#define DLCR30M_DSCR	  						0x100			//R/W
+	#define DLCR30M_DSCR_PI							(1 << 0)
+	#define DLCR30M_DSCR_SE							(1 << 1)
+	#define DLCR30M_DSCR_BRK						(1 << 2)
+	#define DLCR30M_DSCR_STP						(1 << 3)
+	#define DLCR30M_DSCR_WT							(1 << 4)
+	#define DLCR30M_DSCR_RUN						(1 << 14)
+#define DLCR30M_SR								0x104			//R/W 16
+#define DLCR30M_IDR								0x108			//R   16
+#define DLCR30M_EFR								0x10C			//R   32
+#define DLCR30M_DSTART							0x10C			//W   32
+#define DLCR30M_IRQR							0x110			//R/W 32
+#define DLCR30M_IMASKR							0x114			//R/W 32
+#define DLCR30M_TMR								0x118			//R/W 32
+#define DLCR30M_ARBR							0x11C			//R/W 16
+#define DLCR30M_PC								0x120			//R/W 16
+#define DLCR30M_SS								0x124			//R/W 16
+#define DLCR30M_LA								0x128			//R/W 16
+#define DLCR30M_CSL								0x12C			//R/W 16
+#define DLCR30M_LC								0x130			//R/W 16
+#define DLCR30M_CSH								0x134			//R/W 16
+#define DLCR30M_SP								0x138			//R/W 16
+#define DLCR30M_SAR0							0x13C			//R/W 16
+#define DLCR30M_CNTR							0x140			//R/W 16
+#define DLCR30M_SAR(num)						(num * 4 + 0x144)//R/W 16
+
+#define DLCR30M_CCR								0x160			//R/W 16
+#define DLCR30M_PDNR							0x164			//R/W 16
+#define DLCR30M_SFR								0x168			//R/W 32
+#define DLCR30M_QMASKR(num)						(num * 4 + 0x170)//R/W 32
+
+#define DLCR30M_A(index)						(index * 4 + 0x80)//R/W 32
+#define DLCR30M_I(index)						(index * 4 + 0xA0)//R/W 32/16??????
+#define DLCR30M_M(index)						(index * 8 + 0xC0)//R/W 32/16?????
+#define DLCR30M_AT								0xE0				//R/W 32
+#define DLCR30M_IT								0xE4				//R/W 16
+#define DLCR30M_MT								0xE8				//R/W 16
+#define DLCR30M_DT								0xEC				//R/W 16
+#define DLCR30M_IVAR							0xFC				//R/W 16
+
+#define DLCR30M_R2L(index)						(index * 2)		 //R/W? 32
+#define DLCR30M_R1L(index)						(0x40 + (index * 2 - 2))//R/W? 32
+#define DLCR30M_AC(index)						(index * 4 + 0x200)
+// #define DLCR30M_PB_ERR_SR 0x340				//R/W? 32
+
+#define DLCR30M_dbDCSR							0x500			//R/W 16
+#define DLCR30M_SMASKR							0x514			//R/W 32
+#define DLCR30M_Cnt_RUN							0x518			//R/W 32
+#define DLCR30M_dbPCa							0x524			//R 16
+#define DLCR30M_dbPCf							0x528			//R 16
+#define DLCR30M_dbPCd							0x52C			//R 16
+#define DLCR30M_dbPCe							0x520			//R 16
+#define DLCR30M_dbPCe1							0x530			//R 16
+#define DLCR30M_dbPCe2							0x534			//R 16
+#define DLCR30M_dbPCe3							0x538			//R 16
+#define DLCR30M_dbSAR0							0x53C			//R/W 16
+#define DLCR30M_dbCNTR							0x540			//R/W 16
+#define DLCR30M_dbSAR(index)					(index * 4 + 0x544) //R/W 16
 
 typedef struct {
 	struct dsp_cluster* cluster;//указател на структуру кластера
